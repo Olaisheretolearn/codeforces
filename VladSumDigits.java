140c10727a3f5f0a4ebdec4f4e64a96c0b645f7b
Olaoluwa Oke <olaoluwa324@gmail.com>
2024-05-26 19:04:09 -0500

    solved more

--- VladSumDigits.java
+++ VladSumDigits.java
@@ -1,37 +1,50 @@
 import java.util.Scanner;
 public class VladSumDigits{
+ static long [] arr = new long[200005 + 1];
     public static void main(String [] args){
+            //precompute 
+            
+            arr[0] = 0;
+
+            for(int i = 1; i < arr.length; i++){
+                arr[i] = arr[i-1] + digits(i);
+            }
+
+
+
         Scanner kb = new Scanner(System.in);
         int numOfTestCases = kb.nextInt();
         kb.nextLine();
-        for(int i = 0; i < numOfTestCases; i++){
 
+
+        for(int i = 0; i < numOfTestCases; i++){
+ 
         
         int number = kb.nextInt();
         kb.nextLine();
+        
+    result(number);
 
-        System.out.println(breaking(number));
     }
-    }
-        public static int breaking(int number){
-            int sum = 0;
-            while(number > 0){
-            if(number <10){
-                sum = number* (number +1)/2;
-            }
+ 
 
-             if(number > 9){
-                sum +=45;
-                number = number - 9;
-                
-             }
+    
+}
+
+   
+ public static void result(int num){
+    System.out.println(arr[num]+ "\n");
+ }
 
 
-
-
-            }
-            
-            return sum;
+    public static long digits(long num){
+        long sum = 0;
+        while(num > 0){
+            long digit = num % 10;
+            sum+=digit;
+            num = num /10;
         }
-
+        return sum;
+    }
+ 
 }
